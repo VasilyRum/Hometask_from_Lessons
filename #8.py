fa13a2bca717a064107ab99b4b1807516175b0c5
VasilyRum <56539887+VasilyRum@users.noreply.github.com>
2020-01-28 21:36:23 +0300

    Update #8.py

--- #8.py
+++ #8.py
@@ -11,14 +11,14 @@ def getDictIntVal():
             if v.isnumeric():
                 break
             else:
-                print('Value should be integer')
+                print('Value should be integer, Try again')
         r[k] = int (v)
     return r
 print("Enter dictionary")
 d = getDictIntVal()
 print (d)
 r = list(d.values())
-print(r)
 r.sort()
+print('Here is highest 3 values in a dictionary')
 for i in range(-1,-4,-1):
     print(r[i])
