4cfc195d7417d332d4768953ff9ab306401c6763
Blace <blace.rum93@gmail.com>
2020-02-13 23:01:22 +0300

    5 task from lesson VI

--- Lesson VI - Exc, Iter and Gen/task_6_5.py
+++ Lesson VI - Exc, Iter and Gen/task_6_5.py
@@ -8,12 +8,12 @@ def chain(*iterables):
     :param iterables: arbitrary number objects
     :return: iterator
     """
-    # chain('ABC', 'DEF') --> A B C D E F
+
     for i in iterables:
         for element in i:
             yield element
 
 
-A = chain(["JO", "PA"])
+A = chain(["one", "two"])
 print(next(A))
 print(next(A))
